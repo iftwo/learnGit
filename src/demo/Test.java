2d143cf3c654c9247910f720fab82ad2438fa769
zq <chdzq2000@163.com>
2018-12-16 16:42:18 +0800

    git第一次修改

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -3,7 +3,10 @@ package demo;
 public class Test {
     public static void main(String[] args) {
         User user = new User();
-        user.setName("第一次上传git");
+        user.setName("第一次修改git");
         System.out.println(user.getName());
+
+
+        System.out.println("===========");
     }
 }
