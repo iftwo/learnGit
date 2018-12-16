6d0046c12376ed5b14e7e6597f70d070a74fe9fe
zq <chdzq2000@163.com>
2018-12-16 16:28:53 +0800

    git第一次提交

---
+++ src/demo/Test.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Test {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("第一次上传git");
+        System.out.println(user.getName());
+    }
+}
